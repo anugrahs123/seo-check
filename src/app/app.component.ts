a7a1aa2fc4674ad95f1963c5577221ab84b42e8b
anugrahs123 <anugrahsasikumar@gmail.com>
2022-12-06 23:45:59 +0530

    first5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'seo-app';
+  constructor(private titleService: Title,
+    private metaTagService: Meta) { }
+
+  ngOnInit() {
+    this.metaTagService.addTags([
+      {name:'keywords',content:'Angular SEO,Angular Universal'},
+      {name:'robots',content:'index,follow'},
+      {name:'viewport',content:'width=device-width,intial-scale:1'},
+      {name:'charset',content:'UTF-8'}
+    ])
+  }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+  constructor(private titleService: Title,
+    private metaTagService: Meta) { }
 
+  ngOnInit() {
+  this.titleService.setTitle("My App: About me");
+    this.metaTagService.updateTag({name: 'description', content: "I'm a developer who writes sometimes."});
+    //TODO: Make Other top-level components do this so the above content doesn't get stuck there!
+  }
 }
